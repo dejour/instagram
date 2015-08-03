1185dc10d0134d97c9aff102fca733208ff2a687
liufei <krystalnumber@gmail.com>
2015-08-03 14:17:34 +0800

    initial commit

---
+++ client/controllers/home.js
@@ -0,0 +1,17 @@
+angular.module('Instagram')
+  .controller('HomeCtrl', function($scope,$window,$rootScope,$auth) {
+
+    $scope.isAuthenticated = function() {
+		return $auth.isAuthenticated();      
+    };
+
+    $scope.linkInstagram = function() {
+    	$auth.link('instagram')
+    		.then(function  (response) {
+    			$window.localStorage.currentUser = JSON.stringify(response.data.us);
+    			$rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+
+    		});
+    };
+
+  });
